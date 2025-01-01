4edaf9d074a931d2824311b09eefd3973e39d3fd
seansie0830 <seansie0830@gmail.com>
2025-01-01 18:23:41 +0800

    vuetify-project-added

---
+++ vuejs/vuetify-todolist/src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-app>
+    <v-app-bar :elevation="2" color="brown-darken-4">
+      <template v-slot:prepend>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </template>
+      <v-app-bar-title>My notebook</v-app-bar-title>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <v-row justify="center">
+        <v-col cols="12" xs="12" sm="8" md="8" lg="6" xl="6">
+          <v-sheet>
+            <v-container>
+              <v-row>
+                <v-col cols="10">
+                  <v-text-field label="請輸入要新增的筆記" v-model="input"></v-text-field>
+                </v-col>
+                <v-col cols="2">
+                  <v-btn size="large" @click="addnote()" color="blue-darken-4">
+                    送出
+                  </v-btn>
+                </v-col>
+              </v-row>
+            </v-container>
+            <v-list>
+              <note v-for="(item,index) in notes" 
+              :content="item" 
+              :idx="index" 
+              @editContent="editContent($event)" 
+              @deleteContent="deleteContent($event)"
+            />
+            </v-list>
+          </v-sheet>
+        </v-col>
+      </v-row>
+      </v-container>
+    </v-main>
+
+  </v-app>
+</template>
+
+<script >
+import note from './components/note.vue'
+  export default{
+    components:{
+      note
+    },
+    data(){
+      return{
+        notes :[],
+        input : ""
+      }
+    },
+    methods:{
+      addnote(){
+        this.notes.push(this.input)
+        console.log(this.notes)
+      },
+      editContent(props){
+        this.notes[props.idx]=props.input
+      },
+      deleteContent(props){
+        this.notes.splice(props,1);
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
---
+++ vuejs/vuetify-todolist/src/components/note.vue
@@ -0,0 +1,45 @@
+<template>
+	<v-list-item>
+		<v-list-item-content>
+			<v-list-item-title>
+				{{ content }}
+			</v-list-item-title>
+		</v-list-item-content>
+		<v-list-action>
+			<v-btn class="ma-2" color="green-darken-4"icon="mdi-pencil" size="x-small" @click="editElement()"></v-btn>
+			<v-btn class="ma-2" color="red-darken-4" icon="mdi-delete" size="x-small" @click="deleteElement()"></v-btn>
+		</v-list-action>
+	</v-list-item>
+</template>
+<script>
+export default{
+	props:{
+		idx:{
+			type:Number,
+			required: true
+		},
+		content:{
+			type:String,
+			required :true
+		}
+	},
+	data(){
+		return{
+			input :""
+		}
+	},
+	methods:{
+		editElement(){
+			let box={}
+			box.idx=this.idx
+			box.input=this.input
+			this.emit("editContent",box)
+		},
+		deleteElement(){
+			this.$emit("deleteContent",this.idx)
+		}
+	}
+}
+</script>
+<style>
+</style>
